c4fb1c8fc560cde2af24f1c840a3ad858bc22535
BogdanChykD@gmail.com <BogdanChykD@gmail.com@5205bea2-4dbf-e10e-3bc0-905e6055b7a6>
2013-05-14 09:45:56 +0000

    Multi-player version debugging expanded; configuration fixed

--- web/multiplayer/nms/getusersid.php
+++ web/multiplayer/nms/getusersid.php
@@ -1,8 +1,17 @@
 <?php
+include('mylib.php');
+$log = new Logging();
+// set path and name of log file (optional)
+$log->lfile('mylog.txt');
+// write message to the log file
+$log->lwrite("GetUsersId php called");
 include('config.inc.php');
 $con = mysql_connect($dbhost, $dblogin, $dbpass);
 if (!$con)
   {
+  // close log file
+	$log->lwrite('Could not connect: ' . mysql_error());
+	$log->lclose();
   die('Could not connect: ' . mysql_error());
   }
 
@@ -10,18 +19,23 @@ mysql_select_db($dbname, $con);
 
 $t = time() - 10;
 $sql="SELECT * FROM gameusers WHERE available = 1 and lastactivity > ".$t;
+$log->lwrite("Current time minus 10 = ".$t."\nsql=".$sql);
 
 $result = mysql_query($sql);
+$log->lwrite("$result = ".$result);
 echo "[";
 if($row = mysql_fetch_array($result))
   {
+  $log->lwrite("if ");
   echo "{id:'" . $row['id'] . "',name:'" . $row['name'] . "',firstmove:'" . $row['firstmove'] . "'}";
   }
 while($row = mysql_fetch_array($result))
   {
+  $log->lwrite("while ");
   echo ",{id:'" . $row['id'] . "',name:'" . $row['name'] . "',firstmove:'" . $row['firstmove'] . "'}";
   }
 echo "]";
 
 mysql_close($con);
+$log->lclose();
 ?>
